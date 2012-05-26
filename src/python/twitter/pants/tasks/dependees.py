c1efd34bd597ed3a5b767a4c75562f3ebfb0a033
jsirois <jsirois@twitter.com>
2012-05-26 11:33:54 -0600

    [PATCH 0296/3839] Support transitive and intransitive checks.
    RB_ID=68349
    
    (sapling split of 0e5d83339515c275821ec56767b0a846c76608e9)

--- src/python/twitter/pants/tasks/dependees.py
+++ src/python/twitter/pants/tasks/dependees.py
@@ -6,6 +6,18 @@ from twitter.pants.base.build_file import BuildFile
 from twitter.pants.tasks.console_task import ConsoleTask
 
 class ReverseDepmap(ConsoleTask):
+  @classmethod
+  def setup_parser(cls, option_group, args, mkflag):
+    super(ReverseDepmap, cls).setup_parser(option_group, args, mkflag)
+    option_group.add_option(mkflag("transitive"), mkflag("transitive", negate=True),
+                            dest="reverse_depmap_transitive", default=False,
+                            action="callback", callback=mkflag.set_bool,
+                            help="[%default] List transitive dependees.")
+
+  def __init__(self, context):
+    ConsoleTask.__init__(self, context)
+    self._transitive = context.options.reverse_depmap_transitive
+
   def console_output(self, targets):
     dependees_by_target = defaultdict(set)
     for buildfile in BuildFile.scan_buildfiles(get_buildroot()):
@@ -20,12 +32,13 @@ class ReverseDepmap(ConsoleTask):
       yield dependant.address
 
   def get_dependants(self, dependees_by_target, targets):
-    dependants = set()
-    core = set(targets)
+    known_dependants = set()
+    check = set(targets)
     while True:
-      for target in core:
-        dependants.add(target)
+      dependants = set(known_dependants)
+      for target in check:
         dependants.update(dependees_by_target[target])
-      core.update(dependants)
-      if core == dependants:
+      check = dependants - known_dependants
+      if not check or not self._transitive:
         return dependants - set(targets)
+      known_dependants = dependants
