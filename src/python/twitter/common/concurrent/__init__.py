db301db1b71e97506d9113c7677e8fd5d2901484
Kevin Sweeney <ksweeney@twitter.com>
2012-07-26 15:30:39 -0700

    [PATCH 1052/3839] * mesosadm.pex tool * twitter.common_internal.app.modules.socks module * fixed DeprecationWarning in twitter.common.concurrent * refactored BUILDs to depend on :chickadee_helper directly instead of all app modules
    RB_ID=76989
    
    (sapling split of 621a7fb8a0b0a6007308f26f198436a94f573e23)

--- src/python/twitter/common/concurrent/__init__.py
+++ src/python/twitter/common/concurrent/__init__.py
@@ -14,4 +14,4 @@
 # limitations under the License.
 # ==================================================================================================
 
-from futures import *
+from concurrent.futures import *
