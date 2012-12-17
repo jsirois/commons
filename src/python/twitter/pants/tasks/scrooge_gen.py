253bba9faf66efa276c11dbf97c1811cafc47034
Robert Nielsen <rnielsen@twitter.com>
2012-12-17 10:41:49 -0800

    [PATCH 2764/3839] AWESOME-1402: Initial implementation of pants scrooge 2.x goal/plugin for birdcage migration
    RB_ID=103775
    
    (sapling split of fc65a6c7637c428e91335ea76a9b9b5108f3f7b7)

--- src/python/twitter/pants/tasks/scrooge_gen.py
+++ src/python/twitter/pants/tasks/scrooge_gen.py
@@ -18,6 +18,7 @@ from __future__ import print_function
 
 import os
 import sys
+import errno
 import re
 import subprocess
 import tempfile
@@ -38,7 +39,21 @@ from twitter.pants.tasks.binary_utils import profile_classpath, runjava
 from twitter.pants.tasks.code_gen import CodeGen
 from twitter.pants.thrift_util import calculate_compile_sources
 
-SCROOGE_MAIN = 'com.twitter.scrooge.Main'
+INFO_FOR_COMPILER = { 'scrooge':   { 'config': 'scrooge-gen',
+                                     'main':   'com.twitter.scrooge.Main',
+                                     'langs':  frozenset(['scala', 'java']) },
+
+                      'scrooge-2': { 'config': 'scrooge-2-gen',
+                                     'main':   'com.twitter.scrooge.Main',
+                                     'langs':  frozenset(['scala']) } }
+
+INFO_FOR_LANG = { 'java':   { 'target_type': JavaLibrary,
+                              'ext':         'java',
+                              'predicate':   is_jvm },
+
+                  'scala':  { 'target_type': ScalaLibrary,
+                              'ext':         'scala',
+                              'predicate':   is_jvm } }
 
 class ScroogeGen(CodeGen):
   class GenInfo(object):
@@ -52,125 +67,121 @@ class ScroogeGen(CodeGen):
                             help="Emit generated code in to this directory.")
 
     option_group.add_option(mkflag("lang"), dest="scrooge_gen_langs",  default=[],
-                            action="append", type="choice", choices=['scala', 'java'],
+                            action="append", type="choice", choices=INFO_FOR_LANG.keys(),
                             help="Force generation of thrift code for these languages.  Both "
                                  "'scala' and 'java' are supported")
 
   def __init__(self, context):
     CodeGen.__init__(self, context)
 
-    self.classpath = profile_classpath('scrooge-gen')
-    self.output_dir = (
-      context.options.scrooge_gen_create_outdir
-      or context.config.get('scrooge-gen', 'workdir')
-    )
-    self.output_dir = os.path.relpath(self.output_dir)
-    self.strict  = context.config.getbool('scrooge-gen', 'strict')
-    self.verbose = context.config.getbool('scrooge-gen', 'verbose')
-
-
-    def create_geninfo(key):
-      gen_info = context.config.getdict('scrooge-gen', key)
-      gen = gen_info['gen']
-      deps = {}
-      for category, depspecs in gen_info['deps'].items():
-        dependencies = OrderedSet()
-        deps[category] = dependencies
-        for depspec in depspecs:
-          dependencies.update(context.resolve(depspec))
-      return ScroogeGen.GenInfo(gen, deps)
-
-    self.gen_java = create_geninfo('java')
-    self.gen_scala = create_geninfo('scala')
-
     self.gen_langs = set(context.options.scrooge_gen_langs)
-    if len(self.gen_langs) > 1:
-      raise TaskError('Multiple Scrooge generation languages not currently supported.')
+    self.compilers = dict()
 
-    for lang in ('java', 'scala'):
-      if self.context.products.isrequired(lang):
-        self.gen_langs.add(lang)
+    for compiler in targets_for_compiler(context.targets(is_gentarget)):
+      compiler_info = self.compilers[compiler] = dict()
+      compiler_config = INFO_FOR_COMPILER[compiler]['config']
+      compiler_info['classpath'] = profile_classpath(compiler_config)
+      compiler_info['outdir'] = (
+        context.options.scrooge_gen_create_outdir
+        or context.config.get(compiler_config, 'workdir')
+        )
+      compiler_info['outdir'] = os.path.relpath(compiler_info['outdir'])
+      compiler_info['strict']  = context.config.getbool(compiler_config, 'strict')
+      compiler_info['verbose'] = context.config.getbool(compiler_config, 'verbose')
 
+      def create_geninfo(key):
+        gen_info = context.config.getdict(compiler_config, key)
+        gen = gen_info['gen']
+        deps = dict()
+        for category, depspecs in gen_info['deps'].items():
+          dependencies = OrderedSet()
+          deps[category] = dependencies
+          for depspec in depspecs:
+            dependencies.update(context.resolve(depspec))
+        return self.GenInfo(gen, deps)
+
+      compiler_info['gen'] = dict()
+
+      for lang in INFO_FOR_COMPILER[compiler]['langs']:
+        compiler_info['gen'][lang] = create_geninfo(lang)
+        if self.context.products.isrequired(lang):
+          self.gen_langs.add(lang)
 
   def invalidate_for(self):
     return self.gen_langs
 
   def is_gentarget(self, target):
-    return isinstance(target, JavaThriftLibrary) and 'scrooge' == target.compiler
+    return is_gentarget(target)
 
   def is_forced(self, lang):
     return lang in self.gen_langs
 
   def genlangs(self):
-    return dict(java=is_jvm, scala=is_jvm)
+    result = dict()
+    for lang in INFO_FOR_LANG:
+      result[lang] = INFO_FOR_LANG[lang]['predicate']
+    return result
 
   def genlang(self, lang, targets):
-    bases, sources = calculate_compile_sources(targets, self.is_gentarget)
+    compiler_targets = targets_for_compiler(targets)
+    for compiler, targets in compiler_targets.items():
+      bases, sources = calculate_compile_sources(targets, self.is_gentarget)
+      compiler_info = self.compilers[compiler]
 
-    safe_mkdir(self.output_dir)
-    try:
-      gen_file_map = mkstempname()
+      safe_mkdir(compiler_info['outdir'])
+      try:
+        gen_file_map = mkstempname()
 
-      args = [
-        '--language', lang,
-        '--dest', self.output_dir,
-        '--gen-file-map', gen_file_map,
-        '--finagle',
-        '--ostrich',
-        ]
+        args = [
+          '--language', lang,
+          '--dest', compiler_info['outdir'],
+          '--gen-file-map', gen_file_map,
+          # TODO(Robert Nielsen): we need both of the following configurable in the BUILD file
+          '--finagle',
+          '--ostrich',
+          ]
 
-      if not self.strict:
-        args.append('--disable-strict')
-      if self.verbose:
+        # TODO(Robert Nielsen): we need --namespace-map configurable in the BUILD file
+
+        if not compiler_info['strict']:
+          args.append('--disable-strict')
+        if compiler_info['verbose']:
           args.append('--verbose')
 
-      for base in bases:
-        args.extend(('--import-path', base))
+        for base in bases:
+          args.extend(('--import-path', base))
 
-      args.extend(sources)
+        args.extend(sources)
 
-      if 0 != runjava(main=SCROOGE_MAIN, classpath=self.classpath, args=args):
-        raise TaskError
+        if 0 != runjava(main=INFO_FOR_COMPILER[compiler]['main'],
+                        classpath=compiler_info['classpath'], args=args):
+          raise TaskError
 
-      self.gen_files_by_source = self.parse_gen_file_map(gen_file_map)
-    finally:
-      if os.path.exists(gen_file_map):
-        os.remove(gen_file_map)
+        compiler_info['gen_files_for_source'] = self.parse_gen_file_map(gen_file_map,
+                                                                        compiler_info['outdir'])
+      finally:
+        if os.path.exists(gen_file_map):
+          os.remove(gen_file_map)
 
   def createtarget(self, lang, gentarget, dependees):
-    if lang == 'java':
-      return self._create_java_target(gentarget, dependees)
-    elif lang == 'scala':
-      return self._create_scala_target(gentarget, dependees)
-    else:
+    if lang not in INFO_FOR_LANG:
       raise TaskError('Unrecognized scrooge gen lang: %s' % lang)
-
-  # TODO(Robert Nielsen): merge these two into one (maybe move to _inject_target()
-  def _create_java_target(self, target, dependees):
     def create_target(files, deps):
-       return self.context.add_new_target(self.output_dir,
-                                          JavaLibrary,
-                                          name=target.id,
-                                          provides=target.provides,
+       return self.context.add_new_target(self.compilers[gentarget.compiler]['outdir'],
+                                          INFO_FOR_LANG[lang]['target_type'],
+                                          name=gentarget.id,
+                                          provides=gentarget.provides,
                                           sources=files,
                                           dependencies=deps)
-    return self._inject_target(target, dependees, self.gen_java, 'java', create_target)
-
-  def _create_scala_target(self, target, dependees):
-    def create_target(files, deps):
-     return self.context.add_new_target(self.output_dir,
-                                        ScalaLibrary,
-                                        name=target.id,
-                                        provides=target.provides,
-                                        sources=files,
-                                        dependencies=deps)
-    return self._inject_target(target, dependees, self.gen_scala, 'scala', create_target)
+    return self._inject_target(gentarget, dependees, self.compilers[gentarget.compiler]['gen'][lang],
+                               lang, create_target)
 
   def _inject_target(self, target, dependees, geninfo, namespace, create_target):
     files = []
     has_service = False
     for source in target.sources:
-      services, genfiles = calculate_gen(os.path.join(target.target_base, source), self.gen_files_by_source)
+      services, genfiles = calculate_gen(os.path.join(target.target_base, source),
+                                         self.compilers[target.compiler]['gen_files_for_source'])
       has_service = has_service or services
       files.extend(genfiles.get(namespace, []))
     deps = geninfo.deps['service' if has_service else 'structs']
@@ -181,16 +192,16 @@ class ScroogeGen(CodeGen):
       dependee.update_dependencies([tgt])
     return tgt
 
-  def parse_gen_file_map(self, gen_file_map):
+  def parse_gen_file_map(self, gen_file_map, outdir):
     d = dict()
     with open(gen_file_map, 'r') as deps:
       for dep in deps.readlines():
         src, cls = dep.strip().split('->')
         src = os.path.relpath(src.strip(), os.path.curdir)
-        cls = os.path.relpath(cls.strip(), self.output_dir)
+        cls = os.path.relpath(cls.strip(), outdir)
 
         if src not in d:
-          d[src] = set([])
+          d[src] = set()
         d[src].add(cls)
     return d
 
@@ -199,7 +210,7 @@ TYPE_PARSER = re.compile(r'^\s*(const|enum|exception|service|struct|union)\s+([^
 
 
 # TODO(John Sirois): consolidate thrift parsing to 1 pass instead of 2
-def calculate_gen(source, gen_files_by_source):
+def calculate_gen(source, gen_files_for_source):
   """Calculates the service types and files generated for the given thrift IDL source.
 
   Returns a tuple of (service types, generated files).
@@ -207,7 +218,7 @@ def calculate_gen(source, gen_files_by_source):
 
   with open(source, 'r') as thrift:
     lines = thrift.readlines()
-    namespaces = {}
+    namespaces = dict()
     types = defaultdict(set)
     for line in lines:
       match = NAMESPACE_PARSER.match(line)
@@ -224,15 +235,27 @@ def calculate_gen(source, gen_files_by_source):
 
     gen_files_by_lang = defaultdict(set)
 
-    gen_files_by_lang['scala'].update(filter_gen_files(source, gen_files_by_source, '.scala'))
-    gen_files_by_lang['java'].update(filter_gen_files(source, gen_files_by_source, '.java'))
+    for lang in INFO_FOR_LANG:
+      gen_files_by_lang[lang].update(filter_gen_files(source, gen_files_for_source,
+                                                      INFO_FOR_LANG[lang]['ext']))
 
     return types['service'], gen_files_by_lang
 
 
-def filter_gen_files(source, gen_files_by_source, ext):
-  for gen_file in gen_files_by_source[source]:
-    if gen_file.endswith(ext):
+def is_gentarget(target):
+  return isinstance(target, JavaThriftLibrary) and INFO_FOR_COMPILER.get(target.compiler)
+
+
+def targets_for_compiler(targets):
+  result = defaultdict(set)
+  for target in targets:
+    result[target.compiler].add(target)
+  return result
+
+
+def filter_gen_files(source, gen_files_for_source, ext):
+  for gen_file in gen_files_for_source[source]:
+    if gen_file.endswith('.' + ext):
       yield gen_file
 
 
@@ -240,6 +263,6 @@ def mkstempname():
   try:
     handle, name = tempfile.mkstemp()
     os.close(handle)
-  except IOError as e:
-    raise TaskError('IOError: errno=%i: "%s"' % (e.errno, e.strerror), file=sys.stderr)
+  except OSError as e:
+    raise TaskError('OSError: errno=%s: "%s"' % (errno.errorcode(e.errno), e.strerror), file=sys.stderr)
   return name
