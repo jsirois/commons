09af8117c355300b4100c4bc807acb67348c8a8b
jsirois <jsirois@twitter.com>
2013-04-24 12:51:21 -0600

    Fixup CacheManagerTest - make the MockTarget a bit more robust by calling superclass constructors to do normal target setup work.

--- tests/python/twitter/pants/testutils/mock_target.py
+++ tests/python/twitter/pants/testutils/mock_target.py
@@ -1,21 +1,22 @@
+from twitter.pants.base import ParseContext
 
 __author__ = 'Ryan Williams'
 
 from twitter.pants.targets import InternalTarget, TargetWithSources
 
-class MockTarget(InternalTarget, TargetWithSources):
 
-  def __init__(self, id, dependencies = None, num_sources = 0):
-    self.id = id
-    self.address = id
-    self.dependencies = dependencies if dependencies else []
-    self.internal_dependencies = self.dependencies
+class MockTarget(InternalTarget, TargetWithSources):
+  def __init__(self, _id, dependencies=None, num_sources=0):
+    with ParseContext.temp():
+      InternalTarget.__init__(self, _id, dependencies)
+      TargetWithSources.__init__(self, _id)
+
     self.num_sources = num_sources
 
   def resolve(self):
     yield self
 
-  def walk(self, work):
+  def walk(self, work, predicate=None):
     work(self)
     for dep in self.dependencies:
       dep.walk(work)
